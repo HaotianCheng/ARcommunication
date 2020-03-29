67b7f9e81db33a4fab0cbe7fbf6ac58c3ec36826
HaotianCheng <hcheng3@bu.edu>
2020-03-29 18:33:19 -0400

    UI

---
+++ AR Object On Hand/Assets/Scripts/Hello.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hello : MonoBehaviour
+{
+    public Text h;
+
+    public void PrintInConsole()
+    {
+        Debug.Log(Hashing(h.text));
+    }
+
+    public string Hashing(string plainText)
+    {
+        return plainText.ToUpper();
+    }
+
+
+
+}
---
+++ AR Object On Hand/Assets/Scripts/Login.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour
+{
+    public InputField playerName;
+    public InputField password;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
