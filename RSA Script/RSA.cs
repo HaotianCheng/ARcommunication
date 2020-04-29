245787e22bc06a6a3eb1cbd0d0b3134df8833fc1
Alex Jeffrey Lin <43394154+alexlin0625@users.noreply.github.com>
2020-04-29 09:56:17 -0400

    Update RSA.cs

--- RSA Script/RSA.cs
+++ RSA Script/RSA.cs
@@ -19,31 +19,31 @@ namespace RSA_TEST
 
         static void Main()
         {
-            // dafault RSA 1024 key can encrypt 117 bytes at most.
-            // with a 2048 RSA key, you can encrypt 245 bytes, the byte array for signature had 128 bytes.
-            // therefore we use 2048 RSA key for encryption/decryption. and default RSA for sign/verify
-            rsa_encrypt = new RSACryptoServiceProvider(2048);
+            // this script is mainly to simulate RSA double layer security by using key pairs to securely complete communication
+            // between User A and B. Also applying Digital Signatature concept to authenticate the identities of sender and 
+            // receiver. Therefore to prevent Man in the middle attack in our project.
+            rsa_encrypt = new RSACryptoServiceProvider();
             rsa = new RSACryptoServiceProvider();
             myAscii = new ASCIIEncoding();
             message = "Hello world, I'm Alex";
 
             // Get the public keyy   
-            string publickey = rsa.ToXmlString(false); // false to get the public key   
-            string privatekey = rsa.ToXmlString(true); // true to get the private key
+            string publickey = rsa.ToXmlString(false); // public key to verify signature   
+            string privatekey = rsa.ToXmlString(true); // private key sign signature
 
-            string pub2048 = rsa_encrypt.ToXmlString(false);
-            string pri2048 = rsa_encrypt.ToXmlString(true);
+            string pub_encrypt = rsa_encrypt.ToXmlString(false); // public key to encrypt
+            string pri_decrypt = rsa_encrypt.ToXmlString(true); // private key to decrypt
 
             // Create Signature
             Signedstring = CreateSignature(message, privatekey); // A signed a signature on message
-            Console.WriteLine("signature: {0}", Signedstring);
-
             // Call the encryption method
-            encryptedData = EncryptText(pub2048, Signedstring); // A encrypt cipher with B's public key 
+            encryptedData = EncryptText(pub_encrypt, message); // A encrypt message with B's public key 
             // Call the decryption methon
-            decryptedData = DecryptData(pri2048, encryptedData); // B decrypt the cupher with own private key
-
-            if (decryptedData == Signedstring && VerifySignature(message, Signedstring, publickey))
+            decryptedData = DecryptData(pri_decrypt, encryptedData); // B decrypt the cipher with own private key
+            
+            // check if the message is decrypted correctly & 
+            // B verify with A's publickey to authenticate A is the real originator
+            if (decryptedData == message && VerifySignature(message, Signedstring, publickey)) 
             {
                 Console.WriteLine("successfully verfied, message: {0}", message);
             }
